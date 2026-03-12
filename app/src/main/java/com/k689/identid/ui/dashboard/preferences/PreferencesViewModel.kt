f44c1813a106ac07e52e25c9d3685a801c2117c0
Nirmanas <nojus.birmanas@gmail.com>
2026-03-12 20:38:46 +0200

    Scrum 41 (#9)

--- app/src/main/java/com/k689/identid/ui/dashboard/preferences/PreferencesViewModel.kt
+++ app/src/main/java/com/k689/identid/ui/dashboard/preferences/PreferencesViewModel.kt
@@ -5,6 +5,7 @@ import com.k689.identid.controller.storage.PrefKeys
 import com.k689.identid.provider.resources.ResourceProvider
 import com.k689.identid.theme.AppLanguage
 import com.k689.identid.theme.AppTheme
+import com.k689.identid.theme.AppTheme.Companion.toUiText
 import com.k689.identid.ui.mvi.MviViewModel
 import com.k689.identid.ui.mvi.ViewEvent
 import com.k689.identid.ui.mvi.ViewSideEffect
@@ -15,6 +16,7 @@ data class State(
     val screenTitle: String,
     val themeLabel: String = "",
     val languageLabel: String = "",
+    val themeOptions: List<Pair<AppTheme, String>> = emptyList(),
     val selectedTheme: AppTheme = AppTheme.SYSTEM,
     val selectedLanguage: AppLanguage = AppLanguage.SYSTEM,
 ) : ViewState
@@ -47,6 +49,7 @@ class PreferencesViewModel(
             screenTitle = resourceProvider.getString(R.string.preferences_screen_title),
             themeLabel = resourceProvider.getString(R.string.preferences_theme_label),
             languageLabel = resourceProvider.getString(R.string.preferences_language_label),
+            themeOptions = AppTheme.entries.map { it to it.toUiText(resourceProvider) },
             selectedTheme = prefKeys.theme.value,
             selectedLanguage = AppLanguage.fromCurrentLocale(resourceProvider.provideContext()),
         )
